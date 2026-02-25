ecdbe5f840e2997a4ae433b494285045b030c44f
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-25 12:36:19 +0530

    Create trending.py

---
+++ backend/routes/trending.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from database import history
+
+router = APIRouter()
+
+@router.get("/")
+async def trending():
+    pipeline = [
+        {"$group": {"_id": "$url", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    return list(history.aggregate(pipeline))
