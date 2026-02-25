0d4a2cd246cf1b0c16c7c512298b041feac0c02b
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-25 12:32:35 +0530

    Create suggestion_service.py

---
+++ backend/services/suggestion_service.py
@@ -0,0 +1,17 @@
+from YouTubeMusic.Search import Search
+
+async def get_suggestions(title: str, current_url: str):
+    keywords = " ".join(title.split()[:4])
+    data = await Search(keywords, limit=8)
+
+    suggestions = []
+    for r in data["main_results"]:
+        if r["url"] != current_url:
+            suggestions.append({
+                "url": r["url"],
+                "title": r["title"],
+                "thumbnail": r["thumbnail"],
+                "channel": r["channel"]
+            })
+
+    return suggestions
