931c0ced8c6617f3ac179b31054ad55a2b32ad9c
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-02-25 13:06:11 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Vexera:Vexera@vexera.wtrsmyc.mongodb.net/?appName=Vexera")
-REDIS_URL = os.getenv("REDIS_URL", "https://accepted-woodcock-22573.upstash.io")
+REDIS_URL = os.getenv("REDIS_URL", "redis://default:AVgtAAIncDIyNjNlODI3MzNkZTc0ZTE1YmZjOTZkZmZiZDIxODk2MnAyMjI1NzM@accepted-woodcock-22573.upstash.io:6379")
 DB_NAME = "abhitube"
 
 JWT_SECRET = os.getenv("JWT_SECRET", "e43f42172ea2fbd677e498373ec0d425e3adcb6df2dc754118bc989a7e599482")
